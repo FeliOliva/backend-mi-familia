e91c19bd83b29489a43c3e78458b9dfbdda09e68
FeliOliva <felitooliva2@gmail.com>
2025-07-25 18:07:15 -0300

    fix de schema y server.js

--- server.js
+++ server.js
@@ -29,13 +29,13 @@ const metodosPagoRoutes = require("./routes/metodosPagoRoutes");
 
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: "*",
     credentials: true,
   })
 );
 app.use(express.json());
 app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "http://localhost:5173");
+  res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Credentials", "true");
   res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
   res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
